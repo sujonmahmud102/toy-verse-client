5b81969b79c52bfd8b941aa69ffebbf8b8c9bf66
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-05-18 14:33:45 +0600

    footer added

--- src/Pages/Layout/Main.jsx
+++ src/Pages/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer';
 
 const Main = () => {
     return (
         <>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </>
     );
 };
