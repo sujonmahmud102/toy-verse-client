e5ec4e25263f7e1e50c5fc6c9887eb3899b46e77
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-05-21 08:19:13 +0600

    data sort by pricing done

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -2,19 +2,32 @@ import React, { useContext, useEffect, useState } from 'react';
 import useTitle from '../../hooks/useTitle';
 import { Authcontext } from '../../AuthProvider/AuthProvider';
 import MyToysTableRow from './MyToysTableRow';
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import axios from 'axios';
 
 const MyToys = () => {
     const { user } = useContext(Authcontext);
     useTitle('My Toys');
     const [myToys, setMyToys] = useState([]);
 
-    const url = `http://localhost:5000/toys?sellerEmail=${user?.email}`
-    // console.log(url)
+    const fetchData = async (sortOrder) => {
+        try {
+            const response = await axios.get(`http://localhost:5000/toys?sellerEmail=${user?.email}&sort=${sortOrder}`);
+            setMyToys(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
     useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setMyToys(data))
-    }, [])
+        fetchData();
+    }, []);
+
+    // handle sort
+    const handleSort = sortOrder => {
+        fetchData(sortOrder);
+    }
+
 
     return (
         <div className='px-16 my-10'>
@@ -26,7 +39,7 @@ const MyToys = () => {
                         <th>Seller</th>
                         <th>Toy Name</th>
                         <th>Sub Category</th>
-                        <th>Price</th>
+                        <th className='flex gap-2'>Price <button onClick={() => handleSort('asc')} className='tooltip' data-tip="Sort Ascending"><FaArrowDown></FaArrowDown></button> <button onClick={() => handleSort('desc')} className='tooltip' data-tip="Sort Descending"><FaArrowUp></FaArrowUp></button></th>
                         <th>Available</th>
                         <th>Actions</th>
                     </tr>
